a7766e54aba5f15f7124f3ad53b70e3926bb7c8a
Joonas Govenius <joonas.govenius@aalto.fi>
2012-09-26 10:45:13 +0300

    Add separate parameters for resistance and voltage measurements.

--- instrument_plugins/Agilent_34410A.py
+++ instrument_plugins/Agilent_34410A.py
@@ -46,18 +46,26 @@ class Agilent_34410A(Instrument):
         self._address = address
         self._visainstrument = visa.instrument(self._address)
 
-        self.add_parameter('aperture',
-            flags=Instrument.FLAG_GETSET, units='s', minval=100e-6, maxval=1, type=types.FloatType)
-        self.add_parameter('autorange',
-            flags=Instrument.FLAG_GETSET, units='bool', type=types.BooleanType)
-        self.add_parameter('range',
-            flags=Instrument.FLAG_GETSET, units='Ohm', minval=100., maxval=1e9, type=types.FloatType)
+        self.add_parameter('measurement_function',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, type=types.StringType)
+        self.add_parameter('res_nplc',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='', minval=0.006, maxval=100, type=types.FloatType)
+        self.add_parameter('res_autorange',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='bool', type=types.BooleanType)
+        self.add_parameter('res_range',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='Ohm', minval=100., maxval=1e9, type=types.FloatType)
+        self.add_parameter('v_nplc',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='', minval=0.006, maxval=100, type=types.FloatType)
+        self.add_parameter('v_autorange',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='bool', type=types.BooleanType)
+        self.add_parameter('v_range',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='V', minval=0.1, maxval=1e3, type=types.FloatType)
         self.add_parameter('offset_compensation',
-            flags=Instrument.FLAG_GETSET, units='bool', type=types.BooleanType)
-        self.add_parameter('null_value',
-            flags=Instrument.FLAG_GETSET, units='Ohm', minval=-1e-20, maxval=1e-20, type=types.FloatType)
-        self.add_parameter('subtract_null_value',
-            flags=Instrument.FLAG_GETSET, units='bool', type=types.BooleanType)
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='bool', type=types.BooleanType)
+        self.add_parameter('res_null_value',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='Ohm', minval=-1e-20, maxval=1e-20, type=types.FloatType)
+        self.add_parameter('subtract_res_null_value',
+            flags=Instrument.FLAG_GETSET|Instrument.FLAG_GET_AFTER_SET, units='bool', type=types.BooleanType)
 
         self.add_function('reset')
         self.add_function ('get_all')
@@ -85,21 +93,21 @@ class Agilent_34410A(Instrument):
 
     def get_reading(self):
         '''
-        Gets the current resistance reading.
+        Gets the current reading.
 
         Input:
             None
 
         Output:
-            resistance in Ohms
+            resistance in Ohms or voltage in Volts
         '''
-        logging.debug(__name__ + ' : getting current resistance reading')
+        logging.debug(__name__ + ' : getting current reading')
         p = self._visainstrument.ask('READ?')
         try:
             return float(p)
         except ValueError:
             print "Could not convert {0} to float.".format(p)
-            return 0.
+            return float('nan')
 		
     def get_all(self):
         '''
@@ -113,14 +121,50 @@ class Agilent_34410A(Instrument):
             None
         '''
         logging.info(__name__ + ' : get all')
-        self.get_aperture()
-        self.get_autorange()
-        self.get_range()
+        self.get_measurement_function()
+        self.get_res_nplc()
+        self.get_res_autorange()
+        self.get_res_range()
+        self.get_v_nplc()
+        self.get_v_autorange()
+        self.get_v_range()
         self.get_offset_compensation()
-        self.get_subtract_null_value()
-        self.get_null_value()
+        self.get_subtract_res_null_value()
+        self.get_res_null_value()
 
-    def do_get_aperture(self):
+    def do_get_measurement_function(self):
+        '''
+	Ask what is being measured.
+
+        Input:
+            None
+
+        Output:
+            function (string) : what is being measured ('RES', 'VOLT:DC')
+        '''
+	r = self._visainstrument.ask('SENS:FUNC?')
+        logging.debug(__name__ + ' : get measurement function: ' + r)
+	r = r.replace('"','')
+	if r == 'VOLT': r = 'VOLT:DC'
+        return r
+
+    def do_set_measurement_function(self, val):
+        '''
+	Set what is being measured.
+
+        Input:
+            function (string) : what is being measured ('RES', 'VOLT:DC')
+
+        Output:
+            None
+        '''
+	if val == 'VOLT': val = 'VOLT:DC'
+        logging.debug(__name__ + ' : set measurement_function to %s' % val)
+	if val not in ['RES', 'VOLT:DC']:
+		raise Exception('Unknown measurement function: %s' % val)
+        self._visainstrument.write('SENS:FUNC "%s"' % val)
+	
+    def do_get_res_nplc(self):
         '''
 
         Input:
@@ -129,10 +173,10 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get aperture')
-        return float(self._visainstrument.ask('SENS:RES:APER?'))
+        logging.debug(__name__ + ' : get res_nplc')
+        return float(self._visainstrument.ask('SENS:RES:NPLC?'))
 
-    def do_set_aperture(self, val):
+    def do_set_res_nplc(self, val):
         '''
 
         Input:
@@ -141,10 +185,10 @@ class Agilent_34410A(Instrument):
         Output:
             None
         '''
-        logging.debug(__name__ + ' : set aperture to %f' % val)
-        self._visainstrument.write('SENS:RES:APER %s' % val)
+        logging.debug(__name__ + ' : set res_nplc to %f' % val)
+        self._visainstrument.write('SENS:RES:NPLC %s' % val)
 
-    def do_get_range(self):
+    def do_get_v_nplc(self):
         '''
 
         Input:
@@ -153,10 +197,34 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get range')
+        logging.debug(__name__ + ' : get v_nplc')
+        return float(self._visainstrument.ask('SENS:VOLT:DC:NPLC?'))
+
+    def do_set_v_nplc(self, val):
+        '''
+
+        Input:
+            len (int) : averaging length
+
+        Output:
+            None
+        '''
+        logging.debug(__name__ + ' : set v_nplc to %f' % val)
+        self._visainstrument.write('SENS:VOLT:DC:NPLC %s' % val)
+
+    def do_get_res_range(self):
+        '''
+
+        Input:
+            None
+
+        Output:
+            len (int) : averaging length
+        '''
+        logging.debug(__name__ + ' : get res_range')
         return float(self._visainstrument.ask('SENS:RES:RANG?'))
 
-    def do_set_range(self, val):
+    def do_set_res_range(self, val):
         '''
 
         Input:
@@ -165,10 +233,34 @@ class Agilent_34410A(Instrument):
         Output:
             None
         '''
-        logging.debug(__name__ + ' : set range to %f' % val)
+        logging.debug(__name__ + ' : set res_range to %f' % val)
         self._visainstrument.write('SENS:RES:RANG %s' % val)
 
-    def do_get_null_value(self):
+    def do_get_v_range(self):
+        '''
+
+        Input:
+            None
+
+        Output:
+            len (int) : averaging length
+        '''
+        logging.debug(__name__ + ' : get v_range')
+        return float(self._visainstrument.ask('SENS:VOLT:DC:RANG?'))
+
+    def do_set_v_range(self, val):
+        '''
+
+        Input:
+            len (int) : averaging length
+
+        Output:
+            None
+        '''
+        logging.debug(__name__ + ' : set v_range to %f' % val)
+        self._visainstrument.write('SENS:VOLT:DC:RANG %s' % val)
+
+    def do_get_res_null_value(self):
         '''
         Null value is subtracted from the measurement (if option turned on).
 
@@ -178,10 +270,10 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get null value')
+        logging.debug(__name__ + ' : get res_null value')
         return float(self._visainstrument.ask('SENS:RES:NULL:VAL?'))
 
-    def do_set_null_value(self, val):
+    def do_set_res_null_value(self, val):
         '''
         Null value is subtracted from the measurement (if option turned on).
 
@@ -191,12 +283,12 @@ class Agilent_34410A(Instrument):
         Output:
             None
         '''
-        logging.debug(__name__ + ' : set null value to %f' % val)
+        logging.debug(__name__ + ' : set res_null value to %f' % val)
         self._visainstrument.write('SENS:RES:NULL:VAL %s' % val)
 
-    def do_get_autorange(self):
+    def do_get_res_autorange(self):
         '''
-        Is autorange on?
+        Is res_autorange on?
 
         Input:
             None
@@ -204,12 +296,13 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get autorange.')
-        return bool(self._visainstrument.ask('SENS:RES:RANG:AUTO?'))
+	r = self._visainstrument.ask('SENS:RES:RANG:AUTO?')
+        logging.debug(__name__ + ' : get res_autorange: ' + r)
+        return bool(int(r))
 
-    def do_set_autorange(self, val):
+    def do_set_res_autorange(self, val):
         '''
-        Sets autorange on/off.
+        Sets res_autorange on/off.
 
         Input:
             len (int) : averaging length
@@ -217,10 +310,36 @@ class Agilent_34410A(Instrument):
         Output:
             None
         '''
-        logging.debug(__name__ + ' : set autorange to %f' % val)
+        logging.debug(__name__ + ' : set res_autorange to %f' % val)
         self._visainstrument.write('SENS:RES:RANG:AUTO %s' % int(val))
-        
 
+    def do_get_v_autorange(self):
+        '''
+        Is v_autorange on?
+
+        Input:
+            None
+
+        Output:
+            len (int) : averaging length
+        '''
+	r = self._visainstrument.ask('SENS:VOLT:DC:RANG:AUTO?')
+        logging.debug(__name__ + ' : get v_autorange: ' + r)
+        return bool(int(r))
+
+    def do_set_v_autorange(self, val):
+        '''
+        Sets v_autorange on/off.
+
+        Input:
+            len (int) : averaging length
+
+        Output:
+            None
+        '''
+        logging.debug(__name__ + ' : set v_autorange to %f' % val)
+        self._visainstrument.write('SENS:VOLT:DC:RANG:AUTO %s' % int(val))
+        
     def do_get_offset_compensation(self):
         '''
 
@@ -230,8 +349,9 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get offset_compensation')
-        return bool(self._visainstrument.ask('SENS:RES:OCOM?'))
+	r = self._visainstrument.ask('SENS:RES:OCOM?')
+        logging.debug(__name__ + ' : get offset_compensation: ' + r)
+        return bool(int(r))
 
     def do_set_offset_compensation(self, val):
         '''
@@ -245,7 +365,7 @@ class Agilent_34410A(Instrument):
         logging.debug(__name__ + ' : set offset_compensation to %f' % val)
         self._visainstrument.write('SENS:RES:OCOM %s' % int(val))
         
-    def do_get_subtract_null_value(self):
+    def do_get_subtract_res_null_value(self):
         '''
 
         Input:
@@ -254,10 +374,11 @@ class Agilent_34410A(Instrument):
         Output:
             len (int) : averaging length
         '''
-        logging.debug(__name__ + ' : get ')
-        return bool(self._visainstrument.ask('SENS:RES:NULL?'))
+	r = self._visainstrument.ask('SENS:RES:NULL?')
+        logging.debug(__name__ + ' : get res_null value subtraction: ' + r)
+        return bool(int(r))
 
-    def do_set_subtract_null_value(self, val):
+    def do_set_subtract_res_null_value(self, val):
         '''
 
         Input:
@@ -266,5 +387,5 @@ class Agilent_34410A(Instrument):
         Output:
             None
         '''
-        logging.debug(__name__ + ' : set subtract_null_value to %f' % val)
+        logging.debug(__name__ + ' : set subtract_res_null_value to %f' % val)
         self._visainstrument.write('SENS:RES:NULL %s' % int(val))
