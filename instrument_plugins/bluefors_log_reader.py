baee11b4360b438bb45243d36d4667d0702a8d8d
Joonas Govenius <joonas.govenius@aalto.fi>
2013-01-24 14:22:11 +0200

    Added cahcing of parsed data (10 second life time). Also renamed pressure, temperature, resistance --> get_pressure,...

--- instrument_plugins/bluefors_log_reader.py
+++ instrument_plugins/bluefors_log_reader.py
@@ -26,6 +26,7 @@ import pytz
 from dateutil import tz
 import os
 import qt
+import time
 
 class bluefors_log_reader(Instrument):
     '''
@@ -91,9 +92,9 @@ class bluefors_log_reader(Instrument):
         self.add_parameter('latest_flow',
             flags=Instrument.FLAG_GET, units='mmol/s', type=types.FloatType)
 
-        self.add_function('temperature')
-        self.add_function('pressure')
-        self.add_function('flow')
+        self.add_function('get_temperature')
+        self.add_function('get_pressure')
+        self.add_function('get_flow')
         self.add_function ('get_all')
         
         self.add_function('base_heater_current_to_t6')
@@ -150,49 +151,71 @@ class bluefors_log_reader(Instrument):
         # self._visainstrument.write('*RST')
         # self.get_all()
 
-    def __interpolate_value_at_time(self, load_data, at_time=None, interpolation_kind='linear'):
+    def __interpolate_value_at_time(self, value_name, load_data, at_time=None, interpolation_kind='linear', cache_life_time=10.):
         '''
         Returns the interpolated value at 'at_time' based on the data loaded by the load_data function.
 
         Input:
-            load_data -- function that loads the desired data as a sequence of pairs [timestamp_as_datetime, value_as_float]
-            at_time   -- datetime object
+            load_data  -- function that loads the desired data as a sequence of pairs [timestamp_as_datetime, value_as_float]
+            at_time    -- datetime object
+            value_name -- the value being queried, e.g. T1, T2, ... P1, P2, ...
+            cache_life_time -- specifies how long previously parsed data is used (in seconds) before reparsing
             
         Output:
             Interpolated value at 'at_time'. Latest value if at_time==None.
         '''
         
-        # TODO: cache the interpolating function and only regenerate it if the file has changed.
-        
         if at_time==None:
             t = datetime.datetime.now(tz.tzlocal())
         else:
             t = at_time
-
+        
+        # Check if a cache file for the given date exists
+        
+        cache_file_name = "%d-%d-%d_%s_bflog.npy" % (t.year, t.month, t.day, value_name)
+        cache_file_path = os.path.join(qt.config.get('tempdir'), cache_file_name)
+        data = None
+        from_cache = False
         try:
-          data = load_data(self._address, t)
-        except:
-            msg = "WARN: Couldn't load data for {0}. Returning NaN".format(str(t))
-            logging.info(msg)
-            print msg
+          if at_time != None and time.time() - os.path.getmtime(cache_file_path) < cache_life_time:
+            with open(cache_file_path, 'rb') as f:
+              data = np.load(f)
+              logging.debug('Loaded cached data from %s.' % (cache_file_path))
+              from_cache = True
+        except Exception as e:
+          # cache file probably doesn't exist
+          logging.debug('Failed to load a cached interpolating function from %s: %s' % (cache_file_path, str(e)))
+
+        if not from_cache:
+          # parse the data log files
+          try:
+            data = load_data(self._address, t)
+          except Exception as e:
+            logging.warn('Could not load %s at %s. Returning NaN. Exception: %s' % (value_name, str(t), str(e)))
             return np.NaN
+                                                    
+          try:
+            with open(cache_file_path, 'wb') as f:
+              np.save(f, data)
+              logging.debug('Cached data in %s.' % (cache_file_path))
+          except Exception as e:
+            logging.debug('Could not dump data in %s: %s' % (cache_file_path, str(e)))
 
         # return the latest data point if nothing was specified.
         if at_time==None:
           if (t - data[-1][0]).total_seconds() > 305:
-            msg = "WARN: last data point from {0} ago.".format(str(t - data[-1][0]))
-            logging.info(msg)
-            print msg
+            logging.warn('last %s point from %s ago.' % (value_name, str(t - data[-1][0])))
           return data[-1][1]
-        
+      
         # create the interpolating function
-        interpolated_val = interpolate.interp1d([ (d[0] - self._UNIX_EPOCH).total_seconds() for d in data ], data[:,1],
-                                                    kind=interpolation_kind, bounds_error=True)
+        interpolating_fn = interpolate.interp1d([ (d[0] - self._UNIX_EPOCH).total_seconds() for d in data ], data[:,1],
+                                                  kind=interpolation_kind, bounds_error=True)
 
+        # finally use the interpolation function
         try:
-            val = interpolated_val((t - self._UNIX_EPOCH).total_seconds())
+            val = interpolating_fn((t - self._UNIX_EPOCH).total_seconds())
         except Exception as e:
-            print 'Could not interpolate value for t={0}. It is probably out of range.'.format(t)
+            logging.warn('Could not interpolate value %s for t=%s: %s' %(value_name, str(t), str(e)))
             raise e
         
         return val
@@ -208,7 +231,7 @@ class bluefors_log_reader(Instrument):
         scalar_input = True
       
       if past_calibration_range:
-        print "WARN: t6 has not been calibrated for heater currents exceeding %.3e Amp." % self._heater_current_to_t6_calibration_ends
+        logging.warn("t6 has not been calibrated for heater currents exceeding %.3e Amp." % self._heater_current_to_t6_calibration_ends)
       
       for deg, coeff in enumerate(self._heater_current_to_t6_polyfit_coefficients):
         t6 += coeff * np.power(current, deg)
@@ -220,6 +243,12 @@ class bluefors_log_reader(Instrument):
       else:            return t6
 
     def temperature(self, channel, t=None):
+      '''
+      Alias for get_temperature(...).
+      '''
+      return self.get_temperature(channel, t)
+
+    def get_temperature(self, channel, t=None):
         '''
         Gets the temperature of channel at time t.
 
@@ -241,7 +270,7 @@ class bluefors_log_reader(Instrument):
             fname = os.path.join(self._address, datestr, 'CH{0} T {1}.log'.format(channel, datestr))
 
             try:
-              logging.debug('Loading temperature data for %s.' % str(tt))
+              logging.debug('Loading T%d data for %s.' % (channel, str(tt)))
               data = np.loadtxt(fname, dtype={'names': ('date', 'time', 'temperature'), 'formats': ('S9', 'S8', 'f')}, delimiter=',')
               # convert the date & time strings to a datetime object
               data = np.array([ [datetime.datetime(int('20'+d[0][7:9]), int(d[0][4:6]), int(d[0][1:3]), int(d[1][0:2]), int(d[1][3:5]), int(d[1][6:8]), tzinfo=tz.tzlocal()), d[2]] for d in data ])
@@ -251,20 +280,27 @@ class bluefors_log_reader(Instrument):
                 all_data = np.concatenate((all_data, data), axis=0)
                
             except Exception as e:
-              logging.debug('Failed to load temperature data for %s. (Normal if in the future.)' % str(tt))
+              if datetime.datetime.now(tz.tzlocal()) > tt: # The exception is normal if tt is in the future.
+                logging.debug('Failed to load temperature data for %s.' % str(tt))
               pass
 
           if all_data == None:
-            msg = 'WARN: No temperature data loaded for t = %s.' % str(t)
-            logging.info(msg)
+            msg = 'No temperature data loaded for t = %s.' % str(t)
+            logging.warn(msg)
             raise Exception(msg)
 
           return all_data
 
 
-        return self.__interpolate_value_at_time(load_temperature_data, t)
+        return self.__interpolate_value_at_time('T%d' % channel, load_temperature_data, t)
 
     def resistance(self, channel, t=None):
+        '''
+        Alias for get_resistance(...)
+        '''
+        return self.get_resistance(channel, t)
+
+    def get_resistance(self, channel, t=None):
         '''
         Gets the resistance of channel at time t.
 
@@ -293,19 +329,26 @@ class bluefors_log_reader(Instrument):
               else:
                 all_data = np.concatenate((all_data, data), axis=0)
             except Exception as e:
-              logging.debug('Failed to load resistance data for %s. (Normal if in the future.)' % str(tt))
+              if datetime.datetime.now(tz.tzlocal()) > tt: # The exception is normal if tt is in the future.
+                logging.debug('Failed to load resistance data for %s.' % str(tt))
               pass
 
           if all_data == None:
-            msg = 'WARN: No resistance data loaded for t = %s.' % str(t)
-            logging.info(msg)
+            msg = 'No resistance data loaded for t = %s.' % str(t)
+            logging.warn(msg)
             raise Exception(msg)
 
           return all_data
 
-        return self.__interpolate_value_at_time(load_resistance_data, t)
-
+        return self.__interpolate_value_at_time('R%d' % channel, load_resistance_data, t)
+        
     def pressure(self, channel, t=None):
+        '''
+        Alias for get_pressure(...).
+        '''
+        return self.get_pressure(channel, t)
+
+    def get_pressure(self, channel, t=None):
         '''
         Gets the pressure of channel at time t.
 
@@ -336,20 +379,27 @@ class bluefors_log_reader(Instrument):
               else:
                 all_data = np.concatenate((all_data, data), axis=0)
             except Exception as e:
-              logging.debug('Failed to load pressure data for %s. (Normal if in the future.)' % str(tt))
+              if datetime.datetime.now(tz.tzlocal()) > tt: # The exception is normal if tt is in the future.
+                logging.debug('Failed to load pressure data for %s.' % str(tt))
               pass
 
           if all_data == None:
-            msg = 'WARN: No pressure data loaded for t = %s.' % str(t)
-            logging.info(msg)
+            msg = 'No pressure data loaded for t = %s.' % str(t)
+            logging.warm(msg)
             raise Exception(msg)
 
           return all_data
 
 	
-        return self.__interpolate_value_at_time(load_pressure_data, t)
+        return self.__interpolate_value_at_time('P%d' % channel, load_pressure_data, t)
 
     def flow(self, t=None):
+        '''
+        Alias for get_flow(...).
+        '''
+        return self.get_flow(t)
+
+    def get_flow(self, t=None):
         '''
         Gets the flow at time t.
 
@@ -377,17 +427,18 @@ class bluefors_log_reader(Instrument):
               else:
                 all_data = np.concatenate((all_data, data), axis=0)
             except Exception as e:
-              logging.debug('Failed to load flow data for %s. (Normal if in the future.)' % str(tt))
+              if datetime.datetime.now(tz.tzlocal()) > tt: # The exception is normal if tt is in the future.
+                logging.debug('Failed to load flow data for %s.' % str(tt))
               pass
 
           if all_data == None:
-            msg = 'WARN: No flow data loaded for t = %s.' % str(t)
-            logging.info(msg)
+            msg = 'No flow data loaded for t = %s.' % str(t)
+            logging.warn(msg)
             raise Exception(msg)
         
           return all_data
 
-        return self.__interpolate_value_at_time(load_flow_data, t)
+        return self.__interpolate_value_at_time('flow', load_flow_data, t)
 
         
     def __time_to_datestr(self, t):
@@ -402,7 +453,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 1 temperature in Kelvin.
         '''
-        return self.temperature(1)
+        return self.get_temperature(1)
 
     def do_get_latest_t2(self):
         '''
@@ -412,7 +463,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 2 temperature in Kelvin.
         '''
-        return self.temperature(2)
+        return self.get_temperature(2)
 
     def do_get_latest_t5(self):
         '''
@@ -422,7 +473,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 5 temperature in Kelvin.
         '''
-        return self.temperature(5)
+        return self.get_temperature(5)
 
     def do_get_latest_t6(self):
         '''
@@ -432,7 +483,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 6 temperature in Kelvin.
         '''
-        return self.temperature(6)
+        return self.get_temperature(6)
 
 
     def do_get_latest_r1(self):
@@ -443,7 +494,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 1 resistance in Ohms.
         '''
-        return self.resistance(1)
+        return self.get_resistance(1)
 
     def do_get_latest_r2(self):
         '''
@@ -453,7 +504,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 2 resistance in Ohms.
         '''
-        return self.resistance(2)
+        return self.get_resistance(2)
 
     def do_get_latest_r5(self):
         '''
@@ -463,7 +514,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 5 resistance in Ohms.
         '''
-        return self.resistance(5)
+        return self.get_resistance(5)
 
     def do_get_latest_r6(self):
         '''
@@ -473,7 +524,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 6 resistance in Ohms.
         '''
-        return self.resistance(6)
+        return self.get_resistance(6)
 
     def do_get_latest_p1(self):
         '''
@@ -483,7 +534,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 1 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(1)
+        return self.get_pressure(1)
         
     def do_get_latest_p2(self):
         '''
@@ -493,7 +544,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 2 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(2)
+        return self.get_pressure(2)
         
     def do_get_latest_p3(self):
         '''
@@ -503,7 +554,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 3 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(3)
+        return self.get_pressure(3)
         
     def do_get_latest_p4(self):
         '''
@@ -513,7 +564,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 4 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(4)
+        return self.get_pressure(4)
         
     def do_get_latest_p5(self):
         '''
@@ -523,7 +574,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 5 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(5)
+        return self.get_pressure(5)
         
     def do_get_latest_p6(self):
         '''
@@ -533,7 +584,7 @@ class bluefors_log_reader(Instrument):
         Output:
             latest channel 6 pressure in mbar. nan if sensor is off.
         '''
-        return self.pressure(6)
+        return self.get_pressure(6)
         
         
     def do_get_latest_flow(self):
@@ -544,4 +595,4 @@ class bluefors_log_reader(Instrument):
         Output:
             latest flow meter reading in mmol/s.
         '''
-        return self.flow()
+        return self.get_flow()
